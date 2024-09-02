c4987f861dee8e7269aba294c14a835d93f12db6
Javier0626 <130872971+Javier0626@users.noreply.github.com>
2024-09-01 20:17:11 -0500

    Sprint 3 Crud de Clientes (#1)

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,6 @@
 use App\Http\Controllers\ClientesController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::get('/clientes', [ClientesController::class, 'index']);
-Route::get('/create', [ClientesController::class, 'create']);
-Route::post('/clientes', [ClientesController::class, 'store']);
+Route::get('/', [ClientesController::class, 'index'])->name('clientes.index');
+Route::resource('clientes', ClientesController::class);
+    
