5be8fb97f2fdd146dbc474fb68b522a11175f5e0
Javier0626 <jadanaga@hotmail.com>
2024-09-07 15:52:56 -0500

    Actualizacion de rutas para navegar

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,30 @@
 <?php
 
+use App\Http\Controllers\AutenticacionController;
 use App\Http\Controllers\ClientesController;
+use App\Http\Controllers\EmpleadosController;
+use App\Http\Controllers\EnviosController;
+use App\Http\Controllers\UsuariosController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ClientesController::class, 'index'])->name('clientes.index');
+Route::get('/', function () {
+    return view('landingPage');
+});
+
+Route::get('login', [AutenticacionController::class, 'index']);
+Route::get('registro', [AutenticacionController::class, 'registro']);
 Route::resource('clientes', ClientesController::class);
-    
+Route::resource('empleados', EmpleadosController::class);
+Route::resource('usuarios', UsuariosController::class);
+Route::resource('envios', EnviosController::class);
+
+
+// Rutas del Dashboard
+Route::get('dashboard', function () {
+    return view('dashboard.index');
+})->name('dashboard');
+
+Route::get('dashboard/clientes', [ClientesController::class, 'index']);
+Route::get('dashboard/usuarios', [UsuariosController::class, 'index']);
+Route::get('dashboard/empleados', [EmpleadosController::class, 'index']);
+Route::get('dashboard/envios', [EnviosController::class, 'index']);
