9e24d536b96a60232fee6d67cb31aa4109682ae6
Javier0626 <jadanaga@hotmail.com>
2024-09-15 22:22:30 -0500

    Alerta en caso de error vista usuarios

--- resources/views/usuarios/crear.blade.php
+++ resources/views/usuarios/crear.blade.php
@@ -22,7 +22,17 @@
     <div class="py-12">
         <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
             <div class="dark:bg-gray-700 overflow-hidden shadow-xl sm:rounded-lg p-6 lg:p-8">
-
+               
+                @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
+                
                 <form action="{{ route('usuarios.store') }}" method="POST" class="max-w-sm mx-auto">
                     @csrf
 
